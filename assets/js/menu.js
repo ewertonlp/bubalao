4ceaa6dad39752d62e24c041be274f8c82950d11
Ewerton Lopes Pereira <ewertonlopespereira@gmail.com>
2022-02-09 20:29:48 -0300

    feat:Header e Footer.

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,60 +1,81 @@
+const menu = document.querySelector(".menu");
+const menu1 = document.querySelector(".menu1");
 
+const menuMain = menu.querySelector(".menu-main");
+const goBack = menu.querySelector(".go-back");
+const menuTrigger = document.querySelector(".mobile-menu-trigger");
+const closeMenu = menu.querySelector(".mobile-menu-close");
+let subMenu;
+menuMain.addEventListener("click", (e) => {
+  if (!menu.classList.contains("active")) {
+    return;
+  }
+  if (e.target.closest(".menu-item-has-children")) {
+    const hasChildren = e.target.closest(".menu-item-has-children");
+    showSubMenu(hasChildren);
+  }
+});
 
- const menu = document.querySelector(".menu");
- const menuMain = menu.querySelector(".menu-main");
- const goBack = menu.querySelector(".go-back");
- const menuTrigger = document.querySelector(".mobile-menu-trigger");
- const closeMenu = menu.querySelector(".mobile-menu-close");
- let subMenu;
- menuMain.addEventListener("click", (e) =>{
- 	if(!menu.classList.contains("active")){
- 		return;
- 	}
-   if(e.target.closest(".menu-item-has-children")){
-   	 const hasChildren = e.target.closest(".menu-item-has-children");
-      showSubMenu(hasChildren);
-   }
- });
- goBack.addEventListener("click",() =>{
- 	 hideSubMenu();
- })
- menuTrigger.addEventListener("click",() =>{
- 	 toggleMenu();
- })
- closeMenu.addEventListener("click",() =>{
- 	 toggleMenu();
- })
- document.querySelector(".menu-overlay").addEventListener("click",() =>{
- 	toggleMenu();
- })
- function toggleMenu(){
- 	menu.classList.toggle("active");
- 	document.querySelector(".menu-overlay").classList.toggle("active");
- }
- function showSubMenu(hasChildren){
-    subMenu = hasChildren.querySelector(".sub-menu");
-    subMenu.classList.add("active");
-    subMenu.style.animation = "slideLeft 0.5s ease forwards";
-    const menuTitle = hasChildren.querySelector("i").parentNode.childNodes[0].textContent;
-    menu.querySelector(".current-menu-title").innerHTML=menuTitle;
-    menu.querySelector(".mobile-menu-head").classList.add("active");
- }
+let subMenu1;
+menuMain.addEventListener("click", (e) => {
+  if (!menu.classList.contains("active")) {
+    return;
+  }
+  if (e.target.closest(".menu-item-has-children-1")) {
+    const hasChildren1 = e.target.closest(".menu-item-has-children-1");
+    showSubMenu(hasChildren1);
+  }
+});
 
- function  hideSubMenu(){  
-    subMenu.style.animation = "slideRight 0.5s ease forwards";
-    setTimeout(() =>{
-       subMenu.classList.remove("active");	
-    },300); 
-    menu.querySelector(".current-menu-title").innerHTML="";
-    menu.querySelector(".mobile-menu-head").classList.remove("active");
- }
- 
- window.onresize = function(){
- 	if(this.innerWidth >991){
- 		if(menu.classList.contains("active")){
- 			toggleMenu();
- 		}
+goBack.addEventListener("click", () => {
+  hideSubMenu();
+});
+menuTrigger.addEventListener("click", () => {
+  toggleMenu();
+});
+closeMenu.addEventListener("click", () => {
+  toggleMenu();
+});
+document.querySelector(".menu-overlay").addEventListener("click", () => {
+  toggleMenu();
+});
+function toggleMenu() {
+  menu.classList.toggle("active");
+  document.querySelector(".menu-overlay").classList.toggle("active");
+}
+function showSubMenu(hasChildren) {
+  subMenu = hasChildren.querySelector(".sub-menu");
+  subMenu.classList.add("active");
+  subMenu.style.animation = "slideLeft 0.5s ease forwards";
+  const menuTitle =
+    hasChildren.querySelector("i").parentNode.childNodes[0].textContent;
+  menu.querySelector(".current-menu-title").innerHTML = menuTitle;
+  menu.querySelector(".mobile-menu-head").classList.add("active");
+}
 
- 	}
- }
+function showSubMenu1(hasChildren1) {
+  subMenu1 = hasChildren1.querySelector(".sub-menu-1");
+  subMenu1.classList.add("active");
+  subMenu1.style.animation = "slideLeft 0.5s ease forwards";
+  const menuTitle1 =
+    hasChildren1.querySelector("i").parentNode.childNodes[0].textContent;
+  menu.querySelector(".current-menu-title").innerHTML = menuTitle1;
+  menu.querySelector(".mobile-menu-head").classList.add("active");
+}
 
+function hideSubMenu() {
+  subMenu.style.animation = "slideRight 0.5s ease forwards";
+  setTimeout(() => {
+    subMenu.classList.remove("active");
+  }, 300);
+  menu.querySelector(".current-menu-title").innerHTML = "";
+  menu.querySelector(".mobile-menu-head").classList.remove("active");
+}
+
+window.onresize = function () {
+  if (this.innerWidth > 991) {
+    if (menu.classList.contains("active")) {
+      toggleMenu();
+    }
+  }
+};
